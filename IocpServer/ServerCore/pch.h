77dd82af948cac15d2da1d7de3c579f42b4b839b
wooseop <crazyz2000@naver.com>
2024-03-07 11:52:07 +0900

    chore:gitignore 예외 - libprotobuf

--- IocpServer/ServerCore/pch.h
+++ IocpServer/ServerCore/pch.h
@@ -3,9 +3,9 @@
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 
 #ifdef _DEBUG
-#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
+#pragma comment(lib, "Protobuf\\libprotobufd.lib")
 #else
-#pragma comment(lib, "Protobuf\\Release\\libprotobuf.lib")
+#pragma comment(lib, "Protobuf\\libprotobuf.lib")
 #endif // _DEBUG
 
 #include "CorePch.h"
