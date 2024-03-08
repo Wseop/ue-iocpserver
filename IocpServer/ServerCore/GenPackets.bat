ca0af96d77092bb134da91e6a11c2b06febe40a8
wooseop <crazyz2000@naver.com>
2024-03-08 11:54:30 +0900

    build:Protobuf 배치파일 수정

--- IocpServer/ServerCore/GenPackets.bat
+++ IocpServer/ServerCore/GenPackets.bat
@@ -3,3 +3,6 @@ pushd %~dp0
 protoc.exe -I=./ --cpp_out=./ ./Protocol.proto
 
 IF ERRORLEVEL 1 PAUSE
+
+XCOPY /Y Protocol.pb.h "../../UE_Client/Source/UE_Client/ServerCore"
+XCOPY /Y Protocol.pb.cc "../../UE_Client/Source/UE_Client/ServerCore"
