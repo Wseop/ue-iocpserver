e9c9c375bedafa283b08af79429c657b214a4e1d
wooseop <crazyz2000@naver.com>
2024-03-10 13:55:39 +0900

    feat:난수 생성기

---
+++ IocpServer/ServerCore/Utils.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <random>
+
+class Utils
+{
+	template<typename T>
+	static inline T GetRandom(T min, T max)
+	{
+		random_device randomDevice;
+		mt19937 generator(randomDevice());
+
+		if constexpr (is_integral_v<T>)
+		{
+			uniform_int_distribution<T> distribution(min, max);
+			return distribution(generator);
+		}
+		else
+		{
+			uniform_real_distribution<T> distribution(min, max);
+			return distribution(generator);
+		}
+	}
+};
+
